0bd7aec9e96addef9df523a32d09ff7eee29bf4f
Miguel Molina <miguelmolina.1990@gmail.com>
2021-02-27 13:36:35 -0500

    update cors configuration

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('./database/connection');
+const cors = require('cors');
 
 const app = express();
 
@@ -11,14 +12,8 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 app.use(morgan('dev'));
 
-// Config headers and CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+// Config CORS
+app.use(cors());
 
 // Routes
 app.use('/api/employees/', require('./routes/employee.routes'));
